1a208dfbcb9deacfba05041c116dbc7e66ee004f
jayeshgajare1390 <jayeshgajare2003@gmail.com>
2025-12-15 19:06:47 +0530

    Implemented the VillaAPI

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("API/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            var villas = new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Ocean View Villa" },
+                new Villa { Id = 2, Name = "Mountain Retreat" }
+            };
+            return villas;
+        }
+    }
+}
