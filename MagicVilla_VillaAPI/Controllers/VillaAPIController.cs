3e77c78b2b9c747c5fb4e10f110590bc54ff3b82
jayeshgajare1390 <jayeshgajare2003@gmail.com>
2025-12-17 13:46:22 +0530

    Added the SeriLogs

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,9 +11,15 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        private readonly ILogger<VillaAPIController> _logger;
+        public VillaAPIController(ILogger<VillaAPIController>  logger)
+        {
+            _logger=logger;
+        }
         [HttpGet]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
+            _logger.LogInformation("Getting all villas");
             return Ok(VillaStore.villaList);
         }
         [HttpGet("id", Name = "GetVilla")]
@@ -26,7 +32,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             if (id == 0)
             {
-
+                _logger.LogError("Get Villa Error with Id " + id);
                 return BadRequest();
             }
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
